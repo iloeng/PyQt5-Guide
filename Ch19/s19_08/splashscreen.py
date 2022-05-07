e43606d9a731d78a078930769b415f98128888cd
Leon <Liangz.org@gmail.com>
2022-05-07 23:42:32 +0800

    feat: add s19_08 section about SDI with QDockWidget

---
+++ Ch19/s19_08/splashscreen.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from PyQt5.QtWidgets import QSplashScreen, QProgressBar
+from PyQt5.QtGui import QPixmap
+class CSplashScreen(QSplashScreen):		
+	progressBar = None
+	def __init__(self, pixmap) :
+		super(CSplashScreen, self).__init__(pixmap)
+		self.progressBar = QProgressBar(self)
+		#设置进度条的位置
+		self.progressBar.setGeometry(0, pixmap.height() - 50, pixmap.width(), 30)
+		#设置进度条的样式
+		self.progressBar.setStyleSheet('''QProgressBar {color:blackfont:30pxtext-align:center } 
+														QProgressBar::chunk {background-color: rgb(200, 160, 16)}''') 		
+		#设置进度条的范围
+		self.progressBar.setRange(0, 100)    
+		#设置进度条的当前进度（默认值）
+		self.progressBar.setValue(0)		
+		
+	def slot_setProgress(self, step):
+		self.progressBar.setValue(step%101)
